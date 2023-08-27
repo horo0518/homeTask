144c2630a5508002d7d5b70a4dd96fb89fe632c0
noa <noagrossman5@gmail.com>
2023-08-27 14:25:15 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,85 @@
+﻿using ConsoleApp1.models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Write("Enter folder path just in english: ");
+        string folderPath = Console.ReadLine();
+
+        Console.Write("Enter file format (JSON or CSV): ");
+        string fileFormat = Console.ReadLine().ToLower();
+
+        using var httpClient = new HttpClient();
+        UserService userService = new UserService(httpClient);
+
+        var apiUrls = new string[]
+        {
+            "https://randomuser.me/api/",
+            "https://jsonplaceholder.typicode.com/users",
+            "https://dummyjson.com/users",
+            "https://reqres.in/api/users"
+        };
+        var allUsers = new List<TargetUser>();
+
+        foreach (var apiUrl in apiUrls)
+        {
+            var usersFromApi = await userService.FetchUsersFromApi(apiUrl);
+            allUsers.AddRange(usersFromApi);
+
+            Console.WriteLine($"Users fetched from {apiUrl}: {usersFromApi.Count}");
+        }
+
+        Console.WriteLine($"Total users from all APIs: {allUsers.Count}");
+
+        // Write data to a file
+        WriteToFile(folderPath, fileFormat, allUsers);
+    }
+
+    static void WriteToFile(string folderPath, string fileFormat, List<TargetUser> users)
+    {
+        if (string.IsNullOrWhiteSpace(folderPath) || !Directory.Exists(folderPath))
+        {
+            Console.WriteLine("Invalid folder path.");
+            return;
+        }
+
+        string filePath = Path.Combine(folderPath, $"users.{fileFormat}");
+
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                if (fileFormat == "json")
+                {
+                    var json = JsonSerializer.Serialize(users, new JsonSerializerOptions { WriteIndented = true });
+                    writer.Write(json);
+                }
+                else if (fileFormat == "csv")
+                {
+                    writer.WriteLine("First Name,Last Name,Email,Source ID");
+                    foreach (var user in users)
+                    {
+                        writer.WriteLine($"{user.FirstName},{user.LastName},{user.Email},{user.SourceId}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid file format.");
+                }
+            }
+
+            Console.WriteLine($"Data written to {filePath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error writing to file: {ex.Message}");
+        }
+    }
+}
---
+++ ConsoleApp1/Services/UserService.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ConsoleApp1.models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class UserService
+{
+    private readonly HttpClient _httpClient;
+
+    public UserService(HttpClient httpClient)
+    {
+        _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+    }
+
+    public async Task<List<TargetUser>> FetchUsersFromApi(string apiUrl)
+    {
+        var response = await _httpClient.GetAsync(apiUrl);
+        response.EnsureSuccessStatusCode();
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+        JToken responseObject = JToken.Parse(responseBody);
+
+        JToken userArrayToken = GetArrayTokenFromResponse(responseObject, apiUrl);
+        List<TargetUser> endUsers = ExtractUsersFromResponse(userArrayToken, apiUrl);
+
+        PrintUserDetails(endUsers);
+
+        return endUsers;
+    }
+
+    private JToken GetArrayTokenFromResponse(JToken response, string apiUrl)
+    {
+        switch (apiUrl)
+        {
+            case "https://randomuser.me/api/":
+                return response["results"];
+            case "https://jsonplaceholder.typicode.com/users":
+                return response;
+            case "https://dummyjson.com/users":
+                return response["users"];
+            case "https://reqres.in/api/users":
+                return response["data"];
+            default:
+                return null;
+        }
+    }
+
+    private List<TargetUser> ExtractUsersFromResponse(JToken userArrayToken, string sourceId)
+    {
+        List<TargetUser> users = new List<TargetUser>();
+
+        if (userArrayToken is JArray userArray)
+        {
+            foreach (JToken userToken in userArray)
+            {
+                string firstName = ExtractFirstName(userToken);
+                string lastName = ExtractLastName(userToken);
+                string email = ExtractEmail(userToken);
+
+                TargetUser user = new TargetUser
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    SourceId = sourceId
+                };
+
+                users.Add(user);
+            }
+        }
+
+        return users;
+    }
+
+    private string ExtractFirstName(JToken userToken)
+    {
+        if (userToken is JObject userObject)
+        {
+            // Check if the "name" field is an object with "first" property
+            var nameObject = userObject["name"];
+            if (nameObject is JObject nameJsonObject)
+            {
+                return nameJsonObject["first"]?.ToString();
+            }
+
+            // If "name" is not an object, try to directly access the "last_name" property
+            return userObject["name"]?.ToString() ?? userObject["first_name"]?.ToString() ?? userObject["firstName"]?.ToString();
+        }
+
+        return null;
+    }
+
+
+    private string ExtractLastName(JToken userToken)
+    {
+        if (userToken is JObject userObject)
+        {
+            // Check if the "name" field is an object with "last" property
+            var nameObject = userObject["name"];
+            if (nameObject is JObject nameJsonObject)
+            {
+                return nameJsonObject["last"]?.ToString();
+            }
+
+            // If "name" is not an object, try to directly access the "last_name" property
+            return userObject["name"]?.ToString() ?? userObject["last_name"]?.ToString()??userObject["lastName"]?.ToString();
+        }
+
+        return null;
+    }
+
+
+
+
+    private string ExtractEmail(JToken userToken)
+{
+    if (userToken is JObject userObject)
+    {
+        return userObject["email"]?.ToString();
+    }
+    return null;
+}
+
+    private void PrintUserDetails(List<TargetUser> users)
+    {
+        foreach (var user in users)
+        {
+            Console.WriteLine($"First Name: {user.FirstName}");
+            Console.WriteLine($"Last Name: {user.LastName}");
+            Console.WriteLine($"Email: {user.Email}");
+            Console.WriteLine($"Source ID: {user.SourceId}");
+            Console.WriteLine("==============");
+        }
+    }
+}
